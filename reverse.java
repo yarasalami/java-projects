32539e73ac2b6f77e2854bad6f3c77c4ddcdc9fd
yara salami <80980659+yarasalami@users.noreply.github.com>
2022-09-01 15:55:42 +0300

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,17 @@
+public int reverse(int x) {
+  // Time Complexity: O(log(x))
+  // Space Complexity: O(1)
+  int rev = 0;
+  while (x != 0) {
+    int pop = x % 10;
+    if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE/10 && pop > 7)){
+      return 0;
+    } 
+    if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE/10 && pop < -8)){
+      return 0;
+    } 
+    rev = rev * 10 + pop;
+    x /= 10;
+  }
+  return rev;    
+}
