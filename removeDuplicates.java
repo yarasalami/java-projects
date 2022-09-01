b9023be5fb9b96a7b9fafdadbddab40af0822826
yara salami <80980659+yarasalami@users.noreply.github.com>
2022-09-01 12:55:07 +0300

    Update removeDuplicates.java

--- removeDuplicates.java
+++ removeDuplicates.java
@@ -1,4 +1,6 @@
 public int removeDuplicates(int[] nums) {
+    // Time complextiy : O(n).
+    // Space complexity : O(1).
     if (nums.length == 0) return 0;
     int i = 0;
     for (int j = 1; j < nums.length; j++) {
